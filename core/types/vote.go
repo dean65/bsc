79d803e3cfaa554ab2c8d95055a864e5c9f8f33f
calmbeing <frank.h668@outlook.com>
2022-02-24 22:44:31 +0800

    update

--- core/types/vote.go
+++ core/types/vote.go
@@ -1,51 +1,34 @@
 package types
 
 import (
-	"sync/atomic"
-
 	"github.com/ethereum/go-ethereum/common"
 )
 
 const (
 	BLSPublicKeyLength = 48
 	BLSSignatureLength = 96
+
+	MaxDiffForkDist = 11 // Maximum allowed backward distance from the chain head
 )
 
 type BLSPublicKey [BLSPublicKeyLength]byte
 type BLSSignature [BLSSignatureLength]byte
+type ValidatorsBitSet uint64
 
 type VoteData struct {
 	BlockNumber uint64
 	BlockHash   common.Hash
 }
 
-type VoteEnvelope struct {
+type VoteRecord struct {
 	VoteAddress BLSPublicKey
 	Signature   BLSSignature
 	Data        VoteData
-
-	// caches
-	hash atomic.Value
 }
 
-type VoteEnvelopes []*VoteEnvelope
-
-// Hash returns the vote hash.
-func (v *VoteEnvelope) Hash() common.Hash {
-	if hash := v.hash.Load(); hash != nil {
-		return hash.(common.Hash)
-	}
-
-	h := v.calcVoteHash()
-	v.hash.Store(h)
-	return h
-}
-
-func (v *VoteEnvelope) calcVoteHash() common.Hash {
-	voteData := struct {
-		VoteAddress BLSPublicKey
-		Signature   BLSSignature
-		Data        VoteData
-	}{v.VoteAddress, v.Signature, v.Data}
-	return rlpHash(voteData)
+type AggVoteRecord struct {
+	VoteAddressSet ValidatorsBitSet
+	AggSignature   BLSSignature
+	Data           VoteData
+	Extra          []byte
 }
